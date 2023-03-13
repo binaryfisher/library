33a256a0a73957c55984aafc361766efac0b05bb
Wilson Yu <wilsonbinaryfisher@gmail.com>
2023-03-13 11:13:08 -0400

    all done

--- script.js
+++ script.js
@@ -3,15 +3,48 @@ const confirmAddBtn = document.querySelector("#confirm-add");
 const cancelAddBtn = document.querySelector("#cancel-add");
 const popUpForm = document.querySelector(".popup-form");
 const inputs = document.querySelector(".form-container").elements;
-const cardsContainer = document.querySelector(".cards-container")
+const cardsContainer = document.querySelector(".cards-container");
+const authorAlert = document.querySelector("#author-alert");
+const titleAlert = document.querySelector("#title-alert");
+const pageAlert = document.querySelector("#page-alert");
 
 const handleAddBook = (event) => {
-    event.preventDefault();
-    var cardDiv = document.createElement("div");
-    cardDiv.className = "card"
-    cardsContainer.appendChild(cardDiv);
+    if(!inputs[0].value){
+        authorAlert.innerHTML = "Please enter a valid name!"
+    }else{
+        authorAlert.innerHTML = "" 
+    }
+    if(!inputs[1].value){
+        titleAlert.innerHTML = "Please enter a valid title!"
+    }else{
+        titleAlert.innerHTML = ""
+    }
+    if(!inputs[1].value || isNaN(inputs[2].value)){
+        pageAlert.innerHTML = "Please enter a valid page number!"
+    }else{
+        pageAlert.innerHTML = "" 
+    }
+    if(inputs[0].value && inputs[1].value && inputs[1].value && !isNaN(inputs[2].value)){
+        var cardDiv = document.createElement("div");
+        var authorDiv = document.createElement("div");
+        var titleDiv = document.createElement("div");
+        var pageDiv = document.createElement("div");
+
+        cardDiv.className = "card"
+        authorDiv.innerHTML = "Author: " + inputs[0].value;
+        titleDiv.innerHTML = "Title: " + inputs[1].value;
+        pageDiv.innerHTML = "Page: " + inputs[2].value;
+
+        cardDiv.appendChild(authorDiv);
+        cardDiv.appendChild(titleDiv);
+        cardDiv.appendChild(pageDiv);
+
+        cardsContainer.appendChild(cardDiv);
+        popUpForm.style.display = "none";
+    }
+    
   
-   
+    event.preventDefault();
   
 }
 
